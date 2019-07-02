0953593cf67f9136925704df499a9c61eb664a25
Jeffrey Asmus <jeffreyasmus@Jeffreys-MacBook-Pro.local>
2019-07-02 15:21:56 -0700

    initial push

---
+++ services/users/project/__init__.py
@@ -0,0 +1,36 @@
+import os
+
+from flask import Flask  # new
+from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+# instantiate the extensions
+db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+
+
+# new
+def create_app():
+
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+    toolbar.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
---
+++ services/users/project/api/users.py
@@ -0,0 +1,84 @@
+from flask import Blueprint, request
+from flask_restful import Resource, Api
+from sqlalchemy import exc
+
+from project import db
+from project.api.models import User
+
+
+users_blueprint = Blueprint('users', __name__)
+api = Api(users_blueprint)
+
+
+class UsersPing(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'message': 'pong!'
+        }
+
+
+class UserList(Resource):
+    def post(self):
+        post_data = request.get_json()
+        response = {
+            'status': 'fail',
+            'message': 'Invalid payload.'
+        }
+        if not post_data:
+            return response, 400
+        username = post_data.get('username')
+        email = post_data.get('email')
+        try:
+            user = User.query.filter_by(email=email).first()
+            if not user:
+                db.session.add(User(username=username, email=email))
+                db.session.commit()
+                response['status'] = 'success'
+                response['message'] = f'{email} was added!'
+                return response, 201
+            else:
+                response['message'] = 'Sorry. That email already exists.'
+                return response, 400
+        except exc.IntegrityError:
+            db.session.rollback()
+            return response, 400
+
+    def get(self):
+        response = {
+            'status': 'success',
+            'data': {
+                'users': [user.to_json() for user in User.query.all()]
+            }
+        }
+        return response, 200
+
+
+class Users(Resource):
+    def get(self, user_id):
+        response = {
+            'status': 'fail',
+            'message': 'User does not exist'
+        }
+        try:
+            user = User.query.filter_by(id=user_id).first()
+            if not user:
+                return response, 404
+            else:
+                response = {
+                    'status': 'success',
+                    'data': {
+                        'id': user.id,
+                        'username': user.username,
+                        'email': user.email,
+                        'active': user.active
+                    }
+                }
+                return response, 200
+        except ValueError:
+            return response, 404
+
+
+api.add_resource(UsersPing, '/users/ping')
+api.add_resource(UserList, '/users')
+api.add_resource(Users, '/users/<user_id>')
